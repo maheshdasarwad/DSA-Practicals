37a407230fa142e23e1119687dcd371a3b297521
Mahesh Dasarwad <maheshdasarwad855@gmail.com>
2025-05-05 20:15:21 +0530

    Update 9Dictionary.cpp

--- 9Dictionary.cpp
+++ 9Dictionary.cpp
@@ -18,159 +18,167 @@ using namespace std;
 class Node {
     public :
         string keyword , meaning;
-        Node *left;
-        Node *right;
+        Node *left , *right;
         int height;
-        Node( string key, string value) {
-            keyword = key;
-            meaning = value;
+        Node(string keyword , string meaning) {
+            this->keyword = keyword;
+            this->meaning = meaning;
             left = right = NULL;
             height = 1;
         }
 };
 
 int height(Node *node) {
-    if(node != NULL) return node->height;
-    else return 0;
+    if(node == NULL) return 0;
+    else return node->height;
 }
 
 int getBalance(Node *node) {
-    if(node != NULL) return height(node->left) - height(node->right);
-    else return 0;
+    if(node == NULL) return 0;
+    else return height(node->left) - height(node->right);
 }
 
-Node *rotateRight(Node *y) {
-    Node *x = y -> left;
-    Node *T2 = x -> right;
+Node *rightRotation(Node *root) {
+    Node *child = root -> left;
+    Node *childRight = child -> right;
 
-    x -> right = y;
-    y -> left = T2;
+    child -> right = root;
+    root -> left = childRight;
 
-    //update height
-    y->height = 1 + max(height(y->left) , height(y->right));
-    x->height = 1 + max(height(x->left) , height(x->right));
+    root -> height = 1 + max(height(root->left) , height(root->right));
+    child -> height = 1 + max(height(child->left) , height(child->right));
 
-    return x;
+    return child;
 }
 
-Node *rotateLeft(Node *x) {
-    Node *y = x -> right;
-    Node *T2 = y -> left;
+Node *leftRotation(Node *root) {
+    Node *child = root -> right;
+    Node *childLeft = child -> left;
 
-    y -> left = x;
-    x -> right = T2;
+    child -> left = root;
+    root -> right = childLeft;
 
-    //update height
-    y->height = 1 + max(height(y->left) , height(y->right));
-    x->height = 1 + max(height(x->left) , height(x->right));
-    return y;
+    child -> height = 1 + max(height(child->left) , height(child->right));
+    root -> height = 1 + max(height(root->left) , height(root->right));
+
+    return child;
 }
 
-Node *insert(Node *root, string key, string value) {
-    if(root == NULL) return new Node(key,value);
+Node *BalanceTree(Node *root) {
+    int balanceFact = getBalance(root);
 
-    if(key < root->keyword) root->left = insert(root->left,key,value);
-    else if(key > root->keyword) root->right = insert(root->right,key,value);
+    //Left - Left
+    if(balanceFact > 1 && getBalance(root->left) >= 0) {
+        //Right rotation of top
+        return rightRotation(root);
+    }
+    //Right - Right
+    if(balanceFact < -1 && getBalance(root->right) <= 0) {
+        //Left rotation of top
+        return leftRotation(root);
+    }
+    //Left - Right
+    else if(balanceFact > 1 && getBalance(root->left) < 0) {
+        Node *middle = root->left; 
+        //left rotation of middle
+        //right rotation of top
+        root->left = leftRotation(middle);
+        return rightRotation(root);
+    }
+    //Right - Left
+    else if(balanceFact < -1 && getBalance(root->right) > 0) {
+        Node *middle = root->right;
+        //Right rotation of middle
+        //Left rotation of top
+        root->right = rightRotation(middle);
+        return leftRotation(root);
+    }
+    else return root;
+}
+
+Node *insert(Node *root, string keyword, string meaning) {
+    if(root == NULL) return new Node(keyword , meaning);
+
+    if(keyword < root->keyword) root->left = insert(root->left, keyword , meaning);
+    else if(keyword > root->keyword) root->right = insert(root->right, keyword , meaning);
     else {
-        cout<<"\nKeyword already exits. "<<endl;
+        cout<<"\nKeyword already exits."<<endl;
         return root;
     }
 
-    //Assign Height
-    root->height = 1 + max(height(root->left) , height(root->right));
+    root->height = 1 + max(height(root->left),height(root->right));
 
-    //Check the balance of tree
-    int balance = getBalance(root);
-    //Right Right case
-    if(balance > 1 && key < root->left->keyword) return rotateRight(root);
-    //Right Right case
-    if(balance < -1 && key > root->left->keyword) return rotateLeft(root);
-    //Left Right case
-    if(balance > 1 && key > root->left->keyword) {
-        root->left = rotateLeft(root->left);
-        return rotateRight(root);
-    }
-    //Right Left case
-    if(balance < -1 && key < root->left->keyword) {
-        root->right = rotateRight(root->right);
-        return rotateLeft(root);
-    }
-
-    return root;
+    return BalanceTree(root);
 
 }
 
-Node *minValueNode(Node *node) {
-    Node *current = node;
-    while(current && current->left) current = current->left;
+Node *minValueNode(Node *root) {
+    Node *current = root;
+    while(current != NULL && current->left != NULL)  
+        current = current->left;
     return current;
 }
 
-Node *deleteNode(Node* root, string key) {
-    if(root == NULL) return root;
-
-    if(key < root->keyword) root->left = deleteNode(root->left , key);
-    else if(key > root->keyword) root->right = deleteNode(root->right , key);
-    else {
-
-        //Node with 0 or 1 child
-        if(root->left == NULL || root->right == NULL) {
-            Node *temp;
-            if(root->left != NULL) temp = root->left;
-            else temp = root->right;
-
-            if(temp == NULL) {
-                temp = root;
-                root=NULL;
-            }
-            else root = temp;
-            delete temp;
-        }
-        //Node with 2 children
-        else {
-            Node *temp = minValueNode(root->right);
-            root->keyword = temp->keyword;
-            root->meaning = temp->meaning;
-            root->right = deleteNode(root->right, temp->keyword);
-        }
-    }
-
-    if(root == NULL) return root;
-
-    //STEP 2 :
-    root->height = 1 + max(height(root->left) , height(root->right));
-
-    //STEP 3 :
-    int balance = getBalance(root);
-
-    //LEFT LEFT CASE
-    if(balance > 1 && getBalance(root->left) >= 0) return rotateRight(root);
-    //LEFT RIGHT CASE
-    if(balance < 1 && getBalance(root->left) < 0) {
-        root->left = rotateLeft(root->left);
-        return rotateRight(root);
-    }
-    //RIGHT LEFT CASE
-    if(balance < -1 && getBalance(root->right) <= 0) {
-        root->right = rotateRight(root->right);
-        return rotateLeft(root);
-    }
-
-    return root;
-}
-
-Node* update(Node *root, string key, string newMeaning) {
+Node *deleteNode(Node *root, string keyword) {
+    //check if exits
     if(root == NULL) {
-        cout<<"\nKeyword not found."<<endl;
+        cout<<"No data extis !!"<<endl;
         return NULL;
     }
-    if( key < root->keyword) root->left = update(root->left , key , newMeaning);
-    else if(key > root->keyword) root->right = update(root->right , key , newMeaning);
-    else root->meaning = newMeaning;
+
+    if(keyword < root->keyword) {
+        root->left = deleteNode(root->left , keyword);
+    }
+    else if(keyword > root->keyword) {
+        root->right = deleteNode(root->right , keyword);
+    }
+    else {
+        //leaf Node 
+        if(root->left == NULL && root->right == NULL) {
+            delete root;
+            return NULL;
+        }
+        //single child
+        else if(root->left == NULL && root->right != NULL) {
+            Node *temp = root->right;
+            delete root;
+            return temp;
+        }
+        else if(root->left != NULL && root->right == NULL) {
+            Node *temp = root->left;
+            delete root;
+            return temp;
+        }
+        //both child
+        else {
+            Node *curr = root->right; //right side smallest element
+            while(curr->left != NULL) curr = curr->left;
+            root->keyword = curr->keyword;
+            root->meaning = curr->meaning;
+            root->right = deleteNode(root->right , curr->keyword);
+        }
+    }
+
+    //update height
+    root->height = 1 + max(height(root->left) , height(root->right));
+
+    //Balance tree
+    return BalanceTree(root);
+}
+
+Node *update(Node *root, string keyword, string meaning) {
+    if(root==NULL) {
+        cout<<"Keyword not found.."<<endl;
+        return NULL;
+    }
+    if(keyword < root->keyword) root->left = update(root->left, keyword, meaning);
+    else if(keyword > root->keyword) root->right = update(root->right, keyword, meaning);
+    else root->meaning = meaning;
     return root;
 }
 
 void displayAscending(Node *root) {
+    //inorder
     if(root != NULL) {
         displayAscending(root->left);
         cout<<root->keyword<<" : "<<root->meaning<<endl;
@@ -186,11 +194,12 @@ void displayDescending(Node *root) {
     }
 }
 
-int findComparisons(Node* root, string key, int count = 0) {
-    if(root != NULL) return count;
+int findComparisons(Node *root , string keyword , int count = 0) {
+    if(root == NULL) return -1;
     count++;
-    if(key == root->keyword) return findComparisons(root->left, key, count);
-    else return findComparisons(root->right, key, count);
+    if(keyword < root->keyword) return findComparisons(root->left ,keyword, count);
+    else if(keyword > root->keyword) return findComparisons(root->right, keyword, count);
+    else return count;
 }
 
 
@@ -206,7 +215,7 @@ int main() {
         cout << "4. Display Ascending"<<endl;
         cout << "5. Display Descending"<<endl;
         cout << "6. Find comparisons for search"<<endl;
-        cout << "0. Exit"<<endl;
+        cout << "7. Exit"<<endl;
         cout << "Enter choice: ";
         cin >> choice;
 
@@ -242,7 +251,7 @@ int main() {
         else if(choice == 6) {
             cout << "\nEnter keyword to search: ";
             cin >> key;
-            cout << "\nComparisons made: " << findComparisons(root, key) << endl;
+            cout << "Comparisons made: " << findComparisons(root, key) << endl;
         }
         else if(choice == 7) {
             cout << "Exiting...\n";
